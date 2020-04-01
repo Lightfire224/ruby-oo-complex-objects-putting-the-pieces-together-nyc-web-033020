ed60599788cc9cd571e18130b440b7f4c0198f1b
Elias Taveras <eliastave12@gmail.com>
2020-04-01 04:11:59 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,53 @@
 # Make your shoe class here!
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def brand
+    @brand 
+  end
+  
+  def color=(yess)
+    @color = yess
+  end
+  
+  def color
+    @color
+  end
+  
+  def size=(new_size)
+    @size = new_size
+  end
+  
+  def size
+    @size
+  end
+  
+  def material=(mats)
+    @material = mats
+  end
+  
+  def material
+    @material
+  end
+  
+  def condition=(cond)
+    @condition = cond
+  end
+  
+  def condition
+    @condition
+  end
+  
+  def cobble=(cob)
+    @cobble = cob
+  end
+  
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+  
+    
+end
